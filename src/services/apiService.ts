26569ad269452adc886940b9fe9a899ce703c045
miaoda <miaoda@baidu.com>
2026-02-19 23:42:16 +0530

    fix api base url for production

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -1,7 +1,10 @@
 import { authService } from './authService';
 import type { Report, MapBounds } from '@/types/report';
 
-const API_BASE_URL = 'http://localhost:8000';
+/*const API_BASE_URL = 'http://localhost:8000';*/
+
+const API_BASE_URL = import.meta.env.VITE_API_URL;
+
 
 /**
  * Create a new damage report with image upload
